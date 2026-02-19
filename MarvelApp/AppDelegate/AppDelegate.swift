2a04dc5ffdaab4e5d651cc6bc2792c57abcdcc7c
mohamed930 <mohammedali12477@gmail.com>
2026-02-19 13:28:08 +0200

    init project and add the needed third-part library

---
+++ MarvelApp/AppDelegate/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  MarvelApp
+//
+//  Created by Mohamed Ali on 19/02/2026.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
